934b92f606b697987a9ff88fe44eb03830b271d6
atulcode404 <pandeyatul1384@gmail.com>
2026-01-19 04:22:13 +0000

    operations

---
+++ operation.js
@@ -0,0 +1,21 @@
+//***********************operations********************************
+let value =3
+let negValue = -value
+//console.log(negValue);
+console.log(2+2)
+ let str 1= "hello"
+ let str 2 ="hitesh"
+ let str3 = str1 +str2
+ console.log(str3);
+ console.log("1"+2);
+ console.log("1"+2 +2);
+ console.log((3+4)*5%3);
+ console.log(+true);
+ let gamecounter = 100
+ game counter++;
+ console.log(gamecounter);
+
+
+
+
+
